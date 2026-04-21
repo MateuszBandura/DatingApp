02daea16c7ed903d103273b3a5f4e65eb253c81c
Mateusz Bandura <m.bandura3@gmail.com>
2026-04-21 12:06:12 +0200

    Basic routing, toasts, basic animations

---
+++ client/src/core/services/init-service.ts
@@ -0,0 +1,17 @@
+import { inject, Injectable } from '@angular/core';
+import { AccountService } from './account-service';
+import { of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class InitService {
+  private accountService = inject(AccountService);
+
+  init() {
+    const userString = localStorage.getItem('user');
+    if (!userString) return;
+    const user = JSON.parse(userString);
+    this.accountService.currentUser.set(user);
+  }
+}
--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -2,28 +2,36 @@ import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../../core/services/account-service';
 import { LoginCreds } from '../../types/user';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { ToastService } from '../../core/services/toast-service';
 
 @Component({
   selector: 'app-nav',
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink, RouterLinkActive],
   templateUrl: './nav.html',
   styleUrl: './nav.css',
 })
 export class Nav {
   protected accountService = inject(AccountService);
+  private toastService = inject(ToastService);
+  private router = inject(Router);
   protected creds = {} as LoginCreds;
 
   login() {
     this.accountService.login(this.creds).subscribe({
-      next: (result) => {
+      next: () => {
         this.creds = {} as LoginCreds;
-        console.log(result);
+        this.router.navigateByUrl('/members');
+        this.toastService.success('Logged in succesfully');
       },
-      error: (error) => console.log(error),
+      error: (error) => {
+        this.toastService.error(error.error);
+      },
     });
   }
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
